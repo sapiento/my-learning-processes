72b14e20df5853defa15256009500c3e58be8e79
sapiento <108158324+sapiento@users.noreply.github.com>
2022-07-24 03:18:05 +0100

    Add files via upload
    simple coding

---
+++ day1.py
@@ -0,0 +1,13 @@
+
+firstname='chiamaka'
+secondname='ruth'
+print(firstname)
+print(secondname)
+
+
+fruits= ['orange','mango','pear']
+fruits.append('pawpaw')
+print(fruits)
+fruits.pop(2)
+print(fruits)
+
